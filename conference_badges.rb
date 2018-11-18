bd17f517dfec93193d6a814e951ada09b01560f5
dwolfe1987 <dwolfe1987@gmail.com>
2018-11-18 21:37:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,16 +2,19 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end 
 
+
 def batch_badge_creator(names_array)
   names_array.collect do |name_tag|
     badge_maker(name_tag)
   end 
 end 
 
+
 def assign_rooms(speakers, rooms)
   
   
 end 
+
 
 def printer
   batch_badge_creator
