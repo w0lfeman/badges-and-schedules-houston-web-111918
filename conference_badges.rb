bac2fcf72ae6af9b3fcd2003ed4aeb3287e53c8b
dwolfe1987 <dwolfe1987@gmail.com>
2018-11-18 21:13:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,12 @@ def batch_badge_creator(names_array)
   badge_maker(names_array)
 end 
 
+def assign_rooms(speakers, rooms)
+  
+  
+end 
+
+def printer
+  batch_badge_creator
+  assign_rooms 
+end 
