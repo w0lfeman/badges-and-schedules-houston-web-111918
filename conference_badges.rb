90f007097a3634c6746b5f67fad22c7ee060e1de
dwolfe1987 <dwolfe1987@gmail.com>
2018-11-18 22:42:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,10 +16,5 @@ def assign_rooms(speakers)
     speakers_array.push("Hello, #{speaker}! You'll be assigned to room #{index}!")
   end
   speakers_array 
-end 
-
-
-def printer
-  batch_badge_creator
-  assign_rooms.each do |attendees|
+  end 
 end 
